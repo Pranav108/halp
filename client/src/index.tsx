77edb2f636e767b04f8f21f3e2cbdf117c2742f5
Ike <barranco.ike@gmail.com>
2020-09-30 05:33:23 -0600

    created redux boiler plate materials

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import allReducers from "./utils/reducers";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import "./index.css";
 
+const store = createStore(allReducers, composeWithDevTools());
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
